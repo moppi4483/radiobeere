3be7b4576ec1935c3a4bd005644b8a76bc25f95c
moppi4483 <moppi4483@users.noreply.github.com>
2022-11-05 20:32:27 +0100

    Update db-connect.php

--- var/www/include/db-connect.php
+++ var/www/include/db-connect.php
@@ -7,4 +7,27 @@ if (!$verbindung) {
 }
 
 mysqli_select_db($verbindung, "radiobeere");
+
+
+
+function getFQDN() {
+  $fqdn = "";
+  
+  $abfrage = "SELECT * FROM settings WHERE name = 'FQDN';";
+  $ergebnis = mysqli_query($verbindung, $abfrage);
+  while($row = mysqli_fetch_object($ergebnis)) {
+    $fqdn = $row->wert;
+  }
+  
+  return $fqdn;
+}
+
+
+
+function setFQDN($fqdn) {
+  $abfrage = "INSERT INTO table (name, wert) VALUES('FQDN', '" . $fqdn . "') ON DUPLICATE KEY UPDATE wert='" . $fqdn . "';";
+  $ergebnis = mysqli_query($verbindung, $abfrage);
+  
+  return $ergebnis;
+}
 ?>
