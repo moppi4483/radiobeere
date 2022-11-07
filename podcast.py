ad9e60c82f2627f3201e07a851e91e35c0e291aa
moppi4483 <moppi4483@users.noreply.github.com>
2022-11-07 20:16:05 +0100

    Update podcast.py

--- podcast.py
+++ podcast.py
@@ -13,10 +13,35 @@ import os
 import login
 
 
-HOST_NAME = 'http://' + socket.gethostname()
 PODCAST_IMG_PATH = '/var/www/img/podcast/'
 PODCAST_PATH = '/var/www/podcast/'
 
+
+def get_baseurl(connection):
+
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(
+                'SELECT wert FROM settings WHERE name="Protokoll"'
+        )
+        row = cursor.fetchone()
+        prot = row[0]
+        if prot != 'https':
+            prot = 'http'
+        
+        
+        cursor.execute(
+                'SELECT wert FROM settings WHERE name="FQDN"'
+        )
+        row = cursor.fetchone()
+        fqdn = row[0]
+        if fqdn == '':
+            fqdn = socket.gethostname()
+        
+        
+        base_url = prot + '://' + fqdn
+        
+        return base_url
+    
 
 def get_station_name(connection, station_alias):
 
@@ -26,22 +51,22 @@ def get_station_name(connection, station_alias):
                 station_alias,)
         )
         row = cursor.fetchone()
-
+        
         return row[0]
 
 
-def get_podcast_img(station_alias):
+def get_podcast_img(station_alias, hostname):
 
     podcast_img = PODCAST_IMG_PATH + station_alias + '.jpg'
     if os.path.isfile(podcast_img) is False:
-        podcast_img = HOST_NAME + '/img/podcast/' + 'default.jpg'
+        podcast_img = hostname + '/img/podcast/' + 'default.jpg'
     else:
-        podcast_img = HOST_NAME + '/img/podcast/' + station_alias + '.jpg'
+        podcast_img = hostname + '/img/podcast/' + station_alias + '.jpg'
 
     return podcast_img
 
 
-def create_podcast_header(station, last_build_date, podcast_img):
+def create_podcast_header(station, last_build_date, podcast_img, hostname):
 
     podcast_header = [
                     '<?xml version=\"1.0\" encoding=\"UTF-8\"?>',
@@ -52,7 +77,7 @@ def create_podcast_header(station, last_build_date, podcast_img):
                     + station
                     + '</title>',
                     '<link>'
-                    + HOST_NAME
+                    + hostname
                     + '</link>',
                     '<description>Hier hörst du alle Sendungen, die du mit'
                     + ' der RadioBeere aufgenommen hast.</description>',
@@ -73,7 +98,7 @@ def create_podcast_header(station, last_build_date, podcast_img):
                     + station
                     + '</title>',
                     '<link>'
-                    + HOST_NAME
+                    + hostname
                     + '</link>',
                     '</image>',
                     '<itunes:subtitle>Hören, wann du willst!'
@@ -96,7 +121,7 @@ def create_podcast_header(station, last_build_date, podcast_img):
 
 
 def create_podcast_item(title, audio_file, length_bytes, guid,
-                        pub_date, podcast_img, station, length):
+                        pub_date, podcast_img, station, length, hostname):
 
     podcast_item = [
                     '<item>',
@@ -104,14 +129,14 @@ def create_podcast_item(title, audio_file, length_bytes, guid,
                     + title
                     + '</title>',
                     '<link>'
-                    + HOST_NAME
+                    + hostname
                     + '/Aufnahmen/'
                     + audio_file
                     + '</link>',
                     '<description>Aufgenommen mit der RadioBeere.'
                     + '</description>',
                     '<enclosure url=\"'
-                    + HOST_NAME
+                    + hostname
                     + '/Aufnahmen/'
                     + audio_file
                     + '\" length=\"'
@@ -131,7 +156,7 @@ def create_podcast_item(title, audio_file, length_bytes, guid,
                     + station
                     + '</title>',
                     '<link>'
-                    + HOST_NAME
+                    + hostname
                     + '</link>',
                     '</image>',
                     '<itunes:author>'
@@ -161,15 +186,16 @@ def main():
             login.DB_PASSWORD, login.DB_DATABASE)) as connection:
 
         station_alias = sys.argv[1]
+        hostname = get_base_url(connection)
 
         station = get_station_name(connection, station_alias)
         last_build_date = formatdate(time.time(), True)
-        podcast_img = get_podcast_img(station_alias)
+        podcast_img = get_podcast_img(station_alias, hostname)
         xml_file = PODCAST_PATH + station_alias + '.xml'
 
         feed = open(xml_file, 'w')
         podcast_header = create_podcast_header(
-                station, last_build_date, podcast_img)
+                station, last_build_date, podcast_img,hostname)
         feed.write(podcast_header)
         feed.close
 
@@ -188,7 +214,7 @@ def main():
                                     station, recording_time),
                                     db_record[4],
                                     db_record[7],
-                                    HOST_NAME
+                                    hostname
                                     + '/podcast/'
                                     + station_alias
                                     + '_'
@@ -196,7 +222,8 @@ def main():
                                     db_record[8],
                                     podcast_img,
                                     station,
-                                    db_record[6]
+                                    db_record[6],
+                                    hostname
                                     )
 
                 feed.write(podcast_item)
