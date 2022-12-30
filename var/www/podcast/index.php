2acde149b15ede5a94ae918ab0766e678bac68bb
moppi4483 <moppi4483@users.noreply.github.com>
2022-12-30 14:46:24 +0100

    Grundlegende Überarbeitung des Skriptes
    Implementierung der Unterstützung weiterer Audioformate (AAC, MP3) und dem damit verbunden Auslesen der ID3-Tags.
    Aus diesem Grund wurde eine Umstellung auf ffmpeg (ffprobe) erforderlich.
    Die Ergebnisse werden in einer Metadaten-Datei gespeichert um einen schnellen Zugriff auf die Informationen zu gewährleisten.

--- var/www/podcast/index.php
+++ var/www/podcast/index.php
@@ -9,8 +9,9 @@
     listing all mp3 files in the same directory.
 */
 
+
 $channeltitle   = file_get_contents('podcastName');
-$channelauthor  = "Podcastbetreiber";
+$channelauthor  = "Thomas Quander";
 /* $sortby sets the order in which tracks are listed. 
    Options: 
    "newest" = newest on top
@@ -21,16 +22,12 @@ $channelauthor  = "Podcastbetreiber";
 */
 $sortby = "filedesc"; 
 
-
-
-$dir = "ServerURL";
+$dir = "http://recorder.athome.anja-und-thomas.de";
 $parts = explode('/',$_SERVER['REQUEST_URI']);
 for ($i = 0; $i < count($parts) - 1; $i++) {
   $dir .= $parts[$i] . "/";
 }
 header('Content-type: text/xml', true);
-
-
 
 print"<?xml version='1.0' encoding='UTF-8'?>
 <rss xmlns:itunes='http://www.itunes.com/DTDs/Podcast-1.0.dtd' version='2.0'>
@@ -46,7 +43,7 @@ print"<?xml version='1.0' encoding='UTF-8'?>
 ";
 /**/
 // read all mp3 files in the directory
-$temp = glob("*.mp3");
+$temp = glob("{*.mp3,*.m4a}", GLOB_BRACE);
 // create array with timestamp.filename as key
 foreach ($temp as $filename) {
   $mp3files[filemtime($filename).$filename] = $filename;
@@ -72,32 +69,47 @@ switch ($sortby) {
 foreach ($mp3files as $filename) {
   // set empty array for metadata
   $iteminfo = array(
-    "TPE1" => "",
-    "TIT2" => "",
-    "TALB" => "",
-    "Filename" => ""
+    "artist" => "",
+    "title" => "",
+    "album" => "",
+    "length" => ""
   );
-  // read id3 from shell command
-  $idtag = explode("\n",shell_exec("id3v2 -R '$filename'"));
-  foreach($idtag as $line) {
-    // to to match key => value from each line
-    preg_match("/((\w+): (.*))/", $line, $results);
-    // if ID3 tag found, results will return four values
-    if(count($results) == 4) {
-      $iteminfo[$results[2]] = $results[3];
-    }
+
+  $filetype = substr($filename, -3);
+  $audiotype = "";
+  switch($filetype) {
+    case "mp3":
+      $audiotype = "audio/mpeg";
+      break;
+    case "m4a":
+      $audiotype = "audio/aac";
+      break;
+    default:
+      $audiotype = "audio/mpeg";
+      break;
   }
-  // if title too short, use filename as title
-  if (strlen($iteminfo['TIT2']) < 2) {
-    $iteminfo['TIT2'] = $filename;
+
+  if (file_exists(substr($filename, 0, -3) . "metadata")) {
+    $trackinfo = explode("\n", file_get_contents(substr($filename, 0, -3) . "metadata"));
+    $iteminfo['length'] = $trackinfo[3];
+    $iteminfo['artist'] = $trackinfo[0];
+    $iteminfo['title'] = $trackinfo[1];
+    $iteminfo['album'] = $trackinfo[2];
+  } else {
+    $iteminfo['length'] = round(shell_exec("ffprobe -i '$filename' -show_entries format=duration -v quiet -of csv='p=0'"));
+    $iteminfo['artist'] = shell_exec("ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 '$filename'");
+    $iteminfo['title'] = shell_exec("ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 '$filename'");
+    $iteminfo['album'] = shell_exec("ffprobe -loglevel error -show_entries format_tags=album -of default=noprint_wrappers=1:nokey=1 '$filename'");
+    file_put_contents(substr($filename, 0, -3) . "metadata", $iteminfo);
   }
+
   print "
   <item>
-    <title>".$iteminfo['TPE1']." - ".$iteminfo['TIT2']."</title>
-    <itunes:author>".$iteminfo['TPE1']."</itunes:author>
-    <itunes:subtitle>".$iteminfo['TALB']."</itunes:subtitle>
-    <description>".$iteminfo['TIT2']." by ".$iteminfo['TPE1']." Recorded on ".date ("r", filemtime($filename))."</description>
-    <enclosure url=\"".$dir.$filename."\" length=\"".filesize($filename)."\" type=\"audio/mpeg\"/>
+    <title>".$iteminfo['artist']." - ".$iteminfo['title']."</title>
+    <itunes:author>".$iteminfo['artist']."</itunes:author>
+    <itunes:subtitle>".$iteminfo['album']."</itunes:subtitle>
+    <description>".$iteminfo['title']." by ".$iteminfo['artist']." Recorded on ".date ("r", filemtime($filename))."</description>
+    <enclosure url=\"".$dir.$filename."\" length=\"".$iteminfo['length']."\" type=\"$audiotype\"/>
     <guid>".$dir.$filename."</guid>
     <pubDate>".date ("r", filemtime($filename))."</pubDate>
   </item>";
